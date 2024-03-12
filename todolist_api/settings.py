78220068f89a3fc77a09a15455cd1a70a9a9eeab
Sergej Schreiber <132712491+SergejSchreiber@users.noreply.github.com>
2024-03-12 08:54:59 +0100

    updated requirements

--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-2^j6h)+oitfuxi8%k*gq^q=%ni_)hd8$qg-!ldac7@ppf_%+dc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sergejrestapi.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    'sergejrestapi.pythonanywhere.com',
+    'localhost',
+    '127.0.0.1'
+    ]
 
 
 # Application definition
